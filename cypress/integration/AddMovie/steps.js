c1120f5ad378cda680ef099da1fd90f2458cfb43
Alberto Beiztegui Casado <albertobeiz@gmail.com>
2022-02-14 22:47:54 +0100

    Refactor to React

--- cypress/integration/AddMovie/steps.js
+++ cypress/integration/AddMovie/steps.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-undef */
 /// <reference types="Cypress" />
 import { Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
 
 Given('I have no movies in my list', () => {});
 
 When('I visit the site', () => {
-  cy.visit('http://localhost:8080/');
+  cy.visit('http://localhost:3000/');
 });
 
 Then('I see an empty list', () => {
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <div>No movies in your list</div>;
+}
+
+export default App;
