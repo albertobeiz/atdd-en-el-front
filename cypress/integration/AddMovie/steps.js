729fce29d9911df9d7926374898c94e374447f24
Alberto Beiztegui Casado <albertobeiz@gmail.com>
2022-02-18 14:38:00 +0100

    Finished Feature

--- cypress/integration/AddMovie/steps.js
+++ cypress/integration/AddMovie/steps.js
@@ -1,6 +1,25 @@
 /* eslint-disable no-undef */
 /// <reference types="Cypress" />
-import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
+import { Given, Then, When, Before } from "cypress-cucumber-preprocessor/steps";
+
+let movies = [];
+
+function addMovie(movieName) {
+  movies.push({ id: movies.length + 1, name: movieName });
+}
+
+Before(() => {
+  movies = [];
+
+  cy.intercept({ url: "/movies/", method: "GET" }, (req) => {
+    req.reply(movies);
+  });
+
+  cy.intercept({ url: "/movies/", method: "POST" }, (req) => {
+    addMovie(req.body.name);
+    req.reply({});
+  }).as("postMovie");
+});
 
 Given("I have no movies in my list", () => {});
 
@@ -9,18 +28,14 @@ When("I visit the site", () => {
 });
 
 When("I add a movie with name {string}", (movieName) => {
-  cy.intercept({ url: "/movies/", method: "POST" }, {}).as("postMovie");
-
   cy.get("input[id=name]").type(movieName);
   cy.get("button[type=submit]").click();
 
-  cy.wait("@postMovie")
-    .its("request.body")
-    .should("deep.equal", JSON.stringify({ name: movieName }));
+  cy.wait("@postMovie").its("request.body").should("deep.equal", { name: movieName });
 });
 
 Then("I see an empty list", () => {
-  cy.contains("No movies in your list");
+  cy.contains("No hay películas añadidas");
 });
 
 Then("I see a list with:", (dataset) => {
--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import AddMovieForm from "./Components/AddMovieForm";
 import MovieList from "./Components/MovieList";
 
 function App() {
+  const [refresh, setRefresh] = useState(false);
+
+  const refreshMovies = () => {
+    setRefresh(true);
+    setTimeout(() => {
+      setRefresh(false);
+    }, 50);
+  };
+
+  useEffect(() => {
+    refreshMovies();
+  }, []);
+
   return (
     <div>
       <AddMovieForm
-        onSubmit={({ name }) =>
-          fetch("/movies/", { method: "POST", body: JSON.stringify({ name }) })
-        }
+        onSubmit={async ({ name }) => {
+          await fetch("/movies/", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name }),
+          });
+          refreshMovies();
+        }}
       />
 
       <MovieList
@@ -16,7 +36,7 @@ function App() {
           const res = await fetch("/movies/");
           return await res.json();
         }}
-        refresh={true}
+        refresh={refresh}
       ></MovieList>
     </div>
   );
